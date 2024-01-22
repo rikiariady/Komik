dc2abed71f870e2158ceb10ccbe5abace485602d
rikiariady <150464586+rikiariady@users.noreply.github.com>
2024-01-22 20:50:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav');
 
-    hamburger.addEventListener('click', function () {
-        nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-    });
-});
